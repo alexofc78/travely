1be4967a852e8d034ff0899c6d5d824f2c6ac75b
Alexandra Oliveros <alexofc@gmail.com>
2019-11-12 00:23:37 -0400

    added cli and set environment

--- lib/environment.rb
+++ lib/environment.rb
@@ -1,4 +1,6 @@
-require "travely/version"
+require_relative "travely/version"
+require_relative "travely/cli"
+#require 'pry'
 
 module Travely
   class Error < StandardError; end
---
+++ lib/travely/cli.rb
@@ -0,0 +1,8 @@
+class Travely::Cli
+    
+    def intro
+        puts "Hello"
+    end
+
+
+end
